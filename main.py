4574955eb17e85172385751d876c79f243afe1b8
Vishal Pattanaik <vishal.pattanaik05@gmail.com>
2024-02-08 15:28:39 +0530

    Fully working and custom naming support

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import boto3
+#this folder will not be found on github. These are my personal credientials to my AWS account
+from credentials import aws_secret_id,aws_secret_key,region
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+instance_type = 't2.micro'
+image_id="ami-03f4878755434977f"
+security_group = "sg-0137aa7387b9c41fa"
+name_of_instance = "vishal_instance_1"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ec2 = boto3.client('ec2', region_name=region, aws_access_key_id=aws_secret_id, aws_secret_access_key=aws_secret_key)
+
+response = ec2.run_instances(
+    ImageId=image_id,
+    InstanceType= instance_type,
+    SecurityGroupIds=['sg-0137aa7387b9c41fa'],
+    KeyName='new_gen',
+    MaxCount = 1,
+    MinCount = 1,  
+    TagSpecifications=[{
+        'ResourceType':'instance',
+        'Tags':[{
+            'Key':'Name',
+            'Value':name_of_instance
+        }]
+    }]
+)
+
+
+instance_id = response['Instances'][0]['InstanceId']
+print('Launched EC2 instance with ID:', instance_id)
